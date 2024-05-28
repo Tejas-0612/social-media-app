33b8a742af9341507f0422b91fcf727f913bfb3e
Tejas <tejashinde0612@gmail.com>
2024-05-28 06:56:03 +0530

    feat:add healthcheck router and controller

--- backend/src/app.js
+++ backend/src/app.js
@@ -16,6 +16,10 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
-//routes here
+// routes import
+import healthCheckRouter from "./routes/healthcheck.route.js";
+
+// routes declaration
+app.use("/api/v1/healthcheck", healthCheckRouter);
 
 export { app };
