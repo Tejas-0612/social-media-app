856d727db9fd281c7e7f014bf74e5af189f9853d
Tejas <tejashinde0612@gmail.com>
2024-06-04 16:36:03 +0530

    refactor:asyncHandler and ApiResponse

--- backend/src/utils/ApiResponse.js
+++ backend/src/utils/ApiResponse.js
@@ -1,9 +1,9 @@
 class ApiResponse {
-  constructor(statusCode, data, message = "sucess") {
+  constructor(statusCode, data, message = "success") {
     this.statusCode = statusCode;
     this.data = data;
     this.message = message;
-    this.sucess = statusCode < 400;
+    this.success = statusCode < 400;
   }
 }
 
--- backend/src/utils/asyncHandler.js
+++ backend/src/utils/asyncHandler.js
@@ -1,7 +1,17 @@
-const asyncHandler = (requestHandler) => {
-  return (req, res, next) => {
-    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
-  };
-};
+// const asyncHandler = (requestHandler) => {
+//   return (req, res, next) => {
+//     Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+//   };
+// };
 
+const asyncHandler = (fn) => async (req, res, next) => {
+  try {
+    await fn(req, res, next);
+  } catch (err) {
+    res.status(err.statusCode || 500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+};
 export { asyncHandler };
