da23d7e37759eef064ae4c60cb6442205b1aeb80
Tejas <tejashinde0612@gmail.com>
2024-07-01 16:43:16 +0530

    refactor:use dotenv package

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,10 @@
 import { app } from "./app.js";
 import connectDB from "./db/index.js";
+import dotenv from "dotenv";
 
-process.loadEnvFile();
+dotenv.config({
+  path: "./.env",
+});
 
 const PORT = process.env.PORT || 8001;
 
--- backend/src/utils/cloudinary.js
+++ backend/src/utils/cloudinary.js
@@ -1,7 +1,7 @@
 import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
 
-process.loadEnvFile();
+// process.loadEnvFile();
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
